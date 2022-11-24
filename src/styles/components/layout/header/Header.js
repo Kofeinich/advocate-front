5b67e41a40c8d22168b19c6f60a997678c82a124
Kofeinich <agentkonch@mail.ru>
2022-11-24 04:18:19 +0300

    feat: Switcher.js added

--- src/styles/components/layout/header/Header.js
+++ src/styles/components/layout/header/Header.js
@@ -1,7 +1,15 @@
 import * as React from 'react'
-import {Flex} from "@chakra-ui/react";
+import {Box, Flex, Text, useColorMode, useColorModeValue, useMediaQuery} from "@chakra-ui/react";
+import Logo from "../../../../svg/logo.svg" ;
+import {Switcher} from "../../switcher/Switcher";
 
 export const Header = () => {
+    const { colorMode } = useColorMode()
+    const [isMobile] = useMediaQuery("(max-width: 768px)")
+
+    const bg = useColorModeValue('#F7F5FB', '#36382E')
+    const color = useColorModeValue('#474747', '#F7F5FB')
+
     return (
         <Flex
             as={'header'}
@@ -11,8 +19,56 @@ export const Header = () => {
             top={0}
             left={0}
             zIndex={1000}
-            bg={'red'}
+            bg={bg}
+            justify={'center'}
+            alignItems={'center'}
+            p={'15px'}
         >
+            <Flex
+                maxW={'1200px'}
+                justify={'space-between'}
+                alignItems={'center'}
+            >
+                <Flex
+                    justify={'left'}
+                >
+                    <Box
+                        p={'0 10px'}
+                        w={'50px'}
+                        h={'31px'}
+                    >
+                        <Logo/>
+                    </Box>
+                    <Flex
+                        alignItems={'center'}
+                        justify={'space-between'}
+                    >
+                        <Text
+                            p={'0 30px'}
+                        >
+                            Моя практика
+                        </Text>
+                        <Text
+                            p={'0 30px'}
+                        >
+                            Моя практика
+                        </Text>
+                        <Text
+                            p={'0 30px'}
+                        >
+                            Оставить заявку
+                        </Text>
+                        <Text
+                            p={'0 30px'}
+                        >
+                            Контакты
+                        </Text>
+                    </Flex>
+                </Flex>
+                <Flex>
+                    <Switcher/>
+                </Flex>
+            </Flex>
 
         </Flex>
     )
--- src/styles/components/switcher/Switcher.js
+++ src/styles/components/switcher/Switcher.js
@@ -1,9 +1,58 @@
-import {Heading, useColorModeValue, useColorMode} from "@chakra-ui/react";
+import {useColorModeValue, useColorMode, Box, Flex, useMediaQuery} from "@chakra-ui/react";
 import * as React from "react";
+import Sun from "../../../svg/sun.svg" ;
+import Moon from "../../../svg/moon.svg" ;
 
 export const Switcher = () => {
-    const { colorMode } = useColorMode()
-    return <Heading as={'h1'} color={`mode.${colorMode}.heading3`}>
-        Hero
-    </Heading>
+    const [isMobile] = useMediaQuery("(max-width: 768px)")
+    const {colorMode, toggleColorMode} = useColorMode()
+    const borderColor = useColorModeValue('#FFC759', '#F7F5FB')
+    const bgSwitcher = useColorModeValue('#F06449', '#474747')
+
+
+    return <Flex
+        borderRadius={'20px'}
+        border={`1px solid ${borderColor}`}
+        bg={'#F7F5FB'}
+        w={isMobile ? '37px' : '50px'}
+        h={isMobile ? '19px' : '26px'}
+        justify={'space-between'}
+        alignItems={'center'}
+        p={'2px'}
+        onClick={toggleColorMode}
+    >
+        {
+            (colorMode === 'light')
+                ?
+                <>
+                    <Box
+                        w={isMobile ? '12px' : '18px'}
+                        h={isMobile ? '12px' : '18px'}
+                    >
+                        <Moon/>
+                    </Box>
+                    <Box
+                        w={isMobile ? '15px' : '22px'}
+                        h={isMobile ? '15px' : '22px'}
+                        bg={bgSwitcher}
+                        borderRadius='50%'
+                    />
+                </>
+                :
+                <>
+                    <Box
+                        w={isMobile ? '15px' : '22px'}
+                        h={isMobile ? '15px' : '22px'}
+                        bg={bgSwitcher}
+                        borderRadius='50%'
+                    />
+                    <Box
+                        w={isMobile ? '15px' : '25px'}
+                        h={isMobile ? '15px' : '25px'}
+                    >
+                        <Sun/>
+                    </Box>
+                </>
+        }
+    </Flex>
 }
